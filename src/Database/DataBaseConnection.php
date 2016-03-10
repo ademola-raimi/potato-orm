bb86e67b36a6408b5bdd67acb1eac8fad6a5065c
Raimi Ademola <demola.nig@gmail.com>
2016-03-11 00:40:08 +0100

    get mockery package, creates test files

--- src/Database/DataBaseConnection.php
+++ src/Database/DataBaseConnection.php
@@ -1,41 +1,53 @@
 <?php
 
+/**
+ * This class is involve in connection to the database 
+ *
+ * @author   Raimi Ademola <ademola.raimi@andela.com>
+ * @copyright: 2016 Andela
+ */
 namespace Demo;
 
 use Dotenv\Dotenv;
- use PDO;
+use PDO;
 
- class DataBaseConnection extends PDO
- {
-     private $servername;
-     private $username;
-     private $password;
-     private $driver;
-     private $dbname;
+class DataBaseConnection extends PDO
+{
+    private $servername;
+    private $username;
+    private $password;
+    private $driver;
+    private $dbname;
+    
+    /**
+     * This is a constructor; a default method  that will be called automatically during class instantiation.
+     */
+    public function __construct()
+    {
+        $this->loadEnv();
 
-     public function __construct()
-     {
-         $this->loadEnv();
+        $this->servername = getenv('DB_HOST');
+        $this->username   = getenv('DB_USERNAME');
+        $this->password   = getenv('DB_PASSWORD');
+        $this->driver     = getenv('DB_DRIVER');
+        $this->dbname     = getenv('DB_NAME');
 
-         $this->servername = getenv('DB_HOST');
-         $this->username = getenv('DB_USERNAME');
-         $this->password = getenv('DB_PASSWORD');
-         $this->driver = getenv('DB_DRIVER');
-         $this->dbname = getenv('DB_NAME');
+        $dns = 'mysql:host='.$this->servername.';dbname='.$this->dbname;
 
-         $dns = 'mysql:host='.$this->servername.';dbname='.$this->dbname;
-
-         $options = [
-          PDO::ATTR_PERSISTENT   => true,
-          PDO::ATTR_ERRMODE      => PDO::ERRMODE_EXCEPTION,
+        $options = [
+            PDO::ATTR_PERSISTENT   => true,
+            PDO::ATTR_ERRMODE      => PDO::ERRMODE_EXCEPTION,
       ];
 
          parent::__construct($dns, $this->username, $this->password, $options);
-     }
-
-     private function loadEnv()
-     {
-         $dotenv = new Dotenv($_SERVER['DOCUMENT_ROOT']);
-         $dotenv->load();
-     }
- }
+    }
+    
+    /**
+     * Load Dotenv to grant getenv() access to environment variables in .env file.
+     */
+    private function loadEnv()
+    {
+        $dotenv = new Dotenv($_SERVER['DOCUMENT_ROOT']);
+        $dotenv->load();
+    }
+}
