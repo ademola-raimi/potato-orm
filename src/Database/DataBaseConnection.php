6a37126b7e34a1a1bb6fce202a7944450a9c54fe
andela-araimi <demola.nig@gmail.com>
2016-03-10 18:43:21 -0500

    Applied fixes from StyleCI

--- src/Database/DataBaseConnection.php
+++ src/Database/DataBaseConnection.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This class is involve in connection to the database 
+ * This class is involve in connection to the database.
  *
  * @author   Raimi Ademola <ademola.raimi@andela.com>
  * @copyright: 2016 Andela
@@ -18,7 +18,7 @@ class DataBaseConnection extends PDO
     private $password;
     private $driver;
     private $dbname;
-    
+
     /**
      * This is a constructor; a default method  that will be called automatically during class instantiation.
      */
@@ -27,10 +27,10 @@ class DataBaseConnection extends PDO
         $this->loadEnv();
 
         $this->servername = getenv('DB_HOST');
-        $this->username   = getenv('DB_USERNAME');
-        $this->password   = getenv('DB_PASSWORD');
-        $this->driver     = getenv('DB_DRIVER');
-        $this->dbname     = getenv('DB_NAME');
+        $this->username = getenv('DB_USERNAME');
+        $this->password = getenv('DB_PASSWORD');
+        $this->driver = getenv('DB_DRIVER');
+        $this->dbname = getenv('DB_NAME');
 
         $dns = 'mysql:host='.$this->servername.';dbname='.$this->dbname;
 
@@ -39,9 +39,9 @@ class DataBaseConnection extends PDO
             PDO::ATTR_ERRMODE      => PDO::ERRMODE_EXCEPTION,
       ];
 
-         parent::__construct($dns, $this->username, $this->password, $options);
+        parent::__construct($dns, $this->username, $this->password, $options);
     }
-    
+
     /**
      * Load Dotenv to grant getenv() access to environment variables in .env file.
      */
