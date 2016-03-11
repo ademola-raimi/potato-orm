1bc35be24cc56a16370deb1198eedca2dd936514
Raimi Ademola <demola.nig@gmail.com>
2016-03-11 01:33:27 +0100

    modify gitignore and create stub folders in test and src folder

---
+++ src/Stub/User.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @author   Raimi Ademola <ademola.raimi@andela.com>
+ * @copyright: 2016 Andela
+ */
+namespace Demo;
+
+class User extends DataBaseModel
+{
+    
+}
--- tests/Stub/User.php
+++ tests/Stub/User.php
@@ -1,10 +1,14 @@
 <?php
+
 /**
  * @author   Raimi Ademola <ademola.raimi@andela.com>
  * @copyright: 2016 Andela
  */
 namespace Demo;
 
+use Demo\DataBaseModel;
+
 class User extends DataBaseModel
 {
+    
 }
